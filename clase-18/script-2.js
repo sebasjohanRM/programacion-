df5124909b002d00b4be4260691331629acbf7fb
johan sebastian <sebasjohan1708@gmail.com>
2026-03-19 21:45:29 -0400

    mi primer script

---
+++ clase-18/script-2.js
@@ -0,0 +1,16 @@
+const numero = 23;
+const numero2 = 37;
+let resultado = (numero + numero2);
+console.log( resultado);
+const hola = 'Hola ';
+const fullname = 'Johan Sebastian';
+const home = 'bienvenido.';
+console.log(hola);
+console.log(fullname);
+console.log(home);
+const mensaje = hola + fullname  + " " + home;
+console.log(mensaje);
+
+
+
+
---
+++ clase-18/script.js
@@ -0,0 +1,6 @@
+const holaTodos = 25;
+let soySebastianHola = 'como estan?';
+console.log('holaTodos');
+console.log(holaTodos);
+console.log(15*8);
+
